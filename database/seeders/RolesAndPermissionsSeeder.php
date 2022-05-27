709424870915379402f6f0e58e79cc3a7db0036f
Dricko29 <dricko0798@gmail.com>
2022-05-28 03:34:09 +0700

    spatie permissions default

---
+++ database/seeders/RolesAndPermissionsSeeder.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\PermissionRegistrar;
+
+class RolesAndPermissionsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Reset cached roles and permissions
+        app()[PermissionRegistrar::class]->forgetCachedPermissions();
+
+        // create permissions
+        Permission::create(['name' => 'admin_akses']);
+
+        // create roles and assign existing permissions
+        $role3 = Role::create(['name' => 'super-admin']);
+        // gets all permissions via Gate::before rule; see AuthServiceProvider
+        
+        $role2 = Role::create(['name' => 'admin']);
+        $role2->givePermissionTo('admin_akses');
+        
+        $role1 = Role::create(['name' => 'operator']);
+
+        // create demo users
+        $user = \App\Models\User::factory()->create([
+            'name' => 'Example Super-Admin User',
+            'email' => 'superadmin@example.com',
+        ]);
+        $user->assignRole($role3);
+        
+        $user = \App\Models\User::factory()->create([
+            'name' => 'Example Admin User',
+            'email' => 'admin@example.com',
+        ]);
+        $user->assignRole($role2);
+        
+        $user = \App\Models\User::factory()->create([
+            'name' => 'Example Operator User',
+            'email' => 'operator@example.com',
+        ]);
+        $user->assignRole($role1);
+
+
+    }
+}
